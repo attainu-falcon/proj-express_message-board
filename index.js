8e7231667ebb14ef76a5045f0a7946768df53c96
Ishan <its.ishan.kumar@gmail.com>
2019-06-21 03:31:07 +0530

    enhance UI and created session

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 
+const users = [
+    {
+    username: "yash",
+    password: "gurgaon" },
+    {
+    username: "ishan",
+    password: "delhi" },
+    {
+    username: "aditya",
+    password: "pune" }
+]
+
 app.use(express.static('public'))
+app.use(express.urlencoded())
+app.use(session({
+    secret: "message board app"
+}))
+
+app.post('/auth', (req, res) => {
+    for (user of users) {
+        if (req.body.username == user.username && req.body.password == user.password) {
+            req.session.loggedIn = "true";
+        }
+    }
+    if (req.session.loggedIn=="true") {
+        res.redirect('/topics');
+    } else
+        res.send(`<script>alert('wrong credentials!');window.location='/login'</script>`);
+});
+
+app.get('/logout',function(req,res){
+    req.session.destroy();
+    res.redirect('/login');
+    
+})
+    
 
 app.get('/signup', function(req, res) {
     res.sendFile('signup.html', { root: __dirname + '/views' });
@@ -12,7 +48,7 @@ app.get('/login', function(req, res) {
 });
 
 app.get('/topics', function(req, res) {
-    res.sendFile('topics.html', { root: __dirname + '/views' });
+    res.sendFile('topics.html ', { root: __dirname + '/views' });
 });
 
 app.get('/leaderboard', function(req, res) {
